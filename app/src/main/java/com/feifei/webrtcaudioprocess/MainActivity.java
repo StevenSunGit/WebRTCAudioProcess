473dbf8ff084c9f2f092329c582630fb33a57e17
sunyunfei <sunyunfei@xiaomi.com>
2020-12-11 16:50:27 +0800

    init code
    Signed-off-by: sunyunfei <sunyunfei@xiaomi.com>

--- app/src/main/java/com/feifei/webrtcaudioprocess/MainActivity.java
+++ app/src/main/java/com/feifei/webrtcaudioprocess/MainActivity.java
@@ -1,15 +1,96 @@
 package com.feifei.webrtcaudioprocess;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private  static String TAG = "MainActivity";
+
+    private final int GrantCode = 1;
+    private Button ns, vad, agc, aec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestPermissions();
+    }
+
+    private void requestPermissions(){
+        List<String> permissionList = new ArrayList<>();
+
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (!permissionList.isEmpty()){
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            requestPermissions(permissions, GrantCode);
+        }else {
+            grantPermission();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case GrantCode :
+                if (grantResults.length > 0){
+                    for (int result : grantResults){
+                        if (result!=PackageManager.PERMISSION_GRANTED){
+                            Log.d(TAG, "必须同意所有权限！");
+                            return;
+                        }
+                    }
+                }
+                grantPermission();
+        }
+    }
+
+    private void grantPermission(){
+        ns = findViewById(R.id.ns);
+        ns.setOnClickListener(this);
+
+        vad = findViewById(R.id.vad);
+        vad.setOnClickListener(this);
+
+        agc = findViewById(R.id.agc);
+        agc.setOnClickListener(this);
+
+        aec = findViewById(R.id.aec);
+        aec.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ns:
+                break;
+            case R.id.vad:
+                break;
+            case R.id.agc:
+                break;
+            case R.id.aec:
+                break;
+            default:
+                break;
+        }
     }
 }
