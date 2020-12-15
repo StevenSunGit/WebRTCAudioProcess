e2e6b262158f516fdc16aa273760e13f34186ecd
sunyunfei <sunyunfei@xiaomi.com>
2020-12-15 10:09:52 +0800

    init code
    Signed-off-by: sunyunfei <sunyunfei@xiaomi.com>

--- jni/src/audio_effect_native.cpp
+++ jni/src/audio_effect_native.cpp
@@ -1,6 +1,3 @@
-#ifndef WEBRTCAUDIOPROCESS_COM_FEIFEI_WEBRTCAUDIOPROCESS_JNI_H
-#define WEBRTCAUDIOPROCESS_COM_FEIFEI_WEBRTCAUDIOPROCESS_JNI_H
-
 #include <android/log.h>
 #include "com_feifei_webrtcaudioprocess_jni_AudioEffect.h"
 
@@ -16,5 +13,3 @@ extern "C" JNIEXPORT jlong JNICALL Java_com_feifei_webrtcaudioprocess_AudioEffec
 extern "C" JNIEXPORT jlong JNICALL Java_com_feifei_webrtcaudioprocess_AudioEffect_AudioEffectInterface_audioProcessingCreate(JNIEnv *env, jobject thiz, jlong extended_filter_id, jlong delay_agnostic_id){
     return 0;
 }
-
-#endif //WEBRTCAUDIOPROCESS_COM_FEIFEI_WEBRTCAUDIOPROCESS_JNI_H
